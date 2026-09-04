41368496031721580023da6a11dfab412eec6981
milosrs <milosribar@yahoo.com>
2026-09-05 00:04:41 +0200

    feat(realm): configure the realm for salon onboarding

--- keycloak/startup.sh
+++ keycloak/startup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
-mkdir -p /opt/keycloak/themes/fejd
-cp -R /opt/keycloak/data/import/themes/fejd /opt/keycloak/themes/
-exec /opt/keycloak/bin/kc.sh start-dev --http-port=8080 --db=postgres --db-url=jdbc:postgresql://db:5432/fejd --db-username=postgres --db-password=postgres --hostname-strict=false --hostname-debug=true
+
+exec /opt/keycloak/bin/kc.sh start-dev \
+  --import-realm \
+  --http-port=8080 \
+  --db=postgres \
+  --db-url=jdbc:postgresql://db:5432/fejd \
+  --db-schema=keycloak \
+  --db-username=postgres \
+  --db-password=postgres \
+  --hostname-strict=false \
+  --hostname-debug=true
